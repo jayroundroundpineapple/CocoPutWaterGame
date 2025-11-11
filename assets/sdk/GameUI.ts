f2bdb6571a069c0e050ec8bc3dd2c70ac6c02dd6
xiangwei <chenxiangwei@toponegames.cn>
2025-11-11 15:36:56 +0800

    1

--- assets/sdk/GameUI.ts
+++ assets/sdk/GameUI.ts
@@ -1,13 +1,17 @@
 import { _decorator, Component, Node } from 'cc';
 import { AdManager } from './adManager';
+import { AdType } from './ad-enums';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameUI')
 export class GameUI extends Component {
     @property(Node)
     private initButton: Node = null;
+    @property(Node)
+    private showrewardBtn: Node = null;
     start() {
         this.initButton.on(Node.EventType.TOUCH_END, this.onInitButtonClick, this);
+        this.showrewardBtn.on(Node.EventType.TOUCH_END, this.onShowRewardButtonClick, this);
     }
 
     onInitButtonClick() {
@@ -20,6 +24,12 @@ export class GameUI extends Component {
                 console.log('Ad Init:', initialized);
             }
         );
+    }
+    onShowRewardButtonClick() {
+        console.log('onShowRewardButtonClick');
+        AdManager.ShowAd(AdType.AD_TYPE_Reward, 'reward_placement',(adtype,adevent,error)=>{
+            console.log('Ad Event:', adtype, adevent, error);
+        });
     }
     update(deltaTime: number) {
         
